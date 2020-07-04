9d5e10c342f32baa0d1930fc12173629e16f833b
sunalpapreja <sunalpapreja@users.noreply.github.com>
2020-07-04 21:55:00 +0530

    second commit

--- server.js
+++ server.js
@@ -2,11 +2,22 @@ const express = require('express');
 const router = require('./Routes/Router');
 const bodyParser = require('body-parser');
 var app = express();
-const PORT = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/",router);
-app.use(express.static('client/build'));
+const PORT = process.env.PORT || 3000;
+if(process.env.NODE_ENV === "production")
+{
+    app.use(express.static('Client/build'));
+    
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'Client','build','index.html'));
+    });
+}
+else
+{
+    app.use(express.static('Client/build'));
+}
 
 
 app.listen(PORT);
