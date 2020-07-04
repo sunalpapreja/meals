33ca95a26bdccf41cfecb337c32daee5d6e43925
sunalpapreja <sunalpapreja@users.noreply.github.com>
2020-07-04 22:09:06 +0530

    fifth commit

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use("/",router);
 const PORT = process.env.PORT || 3000;
 if(process.env.NODE_ENV === "production")
 {
-    app.use(express.static('Client/build'));
+    app.use(express.static('client/build'));
     
     app.get('*',(req,res)=>{
         res.sendFile(path.join(__dirname,'client','build','index.html'));
